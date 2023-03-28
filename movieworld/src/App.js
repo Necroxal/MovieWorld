aa789b318b15be8472a33eb97492748abf1324f5
German Rangel <gnrg_74@hotmail.com>
2023-03-28 12:43:24 -0600

    Adapting layout to React

--- movieworld/src/App.js
+++ movieworld/src/App.js
@@ -3,22 +3,74 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="layout">
+   
+    <header className="header">
+        <div className="logo">
+            <div className="play"></div>
+        </div>
+        <h1>Movie World</h1>
+    </header>
+    
+    <nav className="nav">
+        <ul>
+            <li><a href="#">Start</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <section className="content">
+        <article className="item-movie">
+            <h3 className="title"> Web Desing</h3>
+            <p className="description">German</p>
+            <button className="edit">Edit</button>
+            <button className="delete">Delete</button>
+
+        </article>
+        <article className="item-movie">
+            <h3 className="title"> Web Desing</h3>
+            <p className="description">German</p>
+            <button className="edit">Edit</button>
+            <button className="delete">Delete</button>
+
+        </article>
+        <article className="item-movie">
+            <h3 className="title"> Web Desing</h3>
+            <p className="description">German</p>
+            <button className="edit">Edit</button>
+            <button className="delete">Delete</button>
+
+        </article>
+        <article className="item-movie">
+            <h3 className="title"> Web Desing</h3>
+            <p className="description">German</p>
+            <button className="edit">Edit</button>
+            <button className="delete">Delete</button>
+        
+        </article>
+    </section>
+    <aside className="side">
+        <div className="search">
+            <h3 className="title">Search</h3>
+            <form>
+                <input type="text"/>
+                <button>Search</button>
+            </form>
+        </div>
+        <div className="add">
+            <h3 className="title">Add Movie</h3>
+            <form>
+                <input type="text" placeholder="Title"/> 
+                <textarea placeholder="Description"></textarea>
+                <input type="submit" value="Save"/>
+            </form>
+        </div>
+    </aside>
+    <footer className="footer">
+        &copy; German Rangel
+    </footer>
+</div>
   );
 }
 

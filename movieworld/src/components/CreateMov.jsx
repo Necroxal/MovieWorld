2e88643063485f3110b02095197a1a1460867a87
German Rangel <gnrg_74@hotmail.com>
2023-03-28 13:52:47 -0600

    create form for add movie

--- movieworld/src/components/CreateMov.jsx
+++ movieworld/src/components/CreateMov.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const CreateMov = () => {
+  const title = 'Add Movie';
+
+  const [movieState,setMovieState] = useState({
+    title: '',
+    description: ''
+  })
+
+  const getFormValues = e =>{
+    e.preventDefault(); 
+   
+    //Get date form
+    let target = e.target;
+    let title = target.title.value;
+    let description = target.description.value;
+
+    //Create Object and save
+    let movie = {
+      id: new Date().getTime,
+      title,
+      description
+    };
+
+    setMovieState(movie);
+    console.log(movieState);
+
+    console.log(movie)
+  }
   return (
      <div className="add">
-            <h3 className="title">Add Movie</h3>
-            <form>
-                <input type="text" placeholder="Title"/> 
-                <textarea placeholder="Description"></textarea>
-                <input type="submit" value="Save"/>
+            <h3 className="title">{title}</h3>
+            <strong>
+                {(movieState.title && movieState.description) && "the movie was created: "+ movieState.title}
+            </strong>
+            <form onSubmit={getFormValues}>
+                <input type="text" 
+                       id="title"
+                       name="title"
+                       placeholder="Title"/> 
+
+                <textarea id="description" 
+                          name="description"
+                          placeholder="Description"></textarea>
+
+                <input type="submit" 
+                       id="save" 
+                       value="Save"/>
             </form>
         </div>
   )
